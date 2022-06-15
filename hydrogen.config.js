1c094fede0cc288bcc5da0b11bee85348ebf1dd0
Mustak9009 <msk58049@gmail.com>
2022-06-15 11:24:57 +0530

    1 file modified

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -1,21 +1,21 @@
-import {defineConfig} from '@shopify/hydrogen/config';
+import { defineConfig } from "@shopify/hydrogen/config";
 import {
   CookieSessionStorage,
   PerformanceMetricsServerAnalyticsConnector,
   ShopifyServerAnalyticsConnector,
-} from '@shopify/hydrogen';
+} from "@shopify/hydrogen";
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
-    storefrontApiVersion: '2022-07',
+    storeDomain: "customiziedwithhydrogen.myshopify.com",
+    storefrontToken: "26d26690b1d1718a534fc479d9a93ea5",
+    storefrontApiVersion: "2022-07",
   },
-  session: CookieSessionStorage('__session', {
-    path: '/',
+  session: CookieSessionStorage("__session", {
+    path: "/",
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
     maxAge: 60 * 60 * 24 * 30,
   }),
   serverAnalyticsConnectors: [
