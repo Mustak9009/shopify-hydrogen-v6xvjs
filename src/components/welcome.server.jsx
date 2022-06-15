ff832b04d566d81bd1801c4c51ac3dc1875bc74e
Mustak9009 <msk58049@gmail.com>
2022-06-15 11:22:29 +0530

    1 file modified

--- src/components/welcome.server.jsx
+++ src/components/welcome.server.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Welcome = () => {
-  return <div className="text-blu-500">Welcome</div>;
+  return <div className="text-blue-500">Welcome</div>;
 };
 
 export default Welcome;
