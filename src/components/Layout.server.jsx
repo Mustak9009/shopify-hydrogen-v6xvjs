96faf90a13a3b27b9998503b236c3cd783b446f1
Mustak9009 <msk58049@gmail.com>
2022-06-17 10:13:44 +0530

    1 file modified

--- src/components/Layout.server.jsx
+++ src/components/Layout.server.jsx
@@ -1,16 +1,32 @@
 import React from "react";
-import { LocalizationProvider, useShop } from "@shopify/hydrogen";
+import {
+  LocalizationProvider,
+  useShop,
+  useShopQuery,
+  gql,
+} from "@shopify/hydrogen";
 import { Suspense } from "react";
 import Header from "./Header.client";
 const Layout = ({ children }) => {
-  const { storefrontApiVersion } = useShop(); //useShop hook provides access to values of (hydrogen.config.js) file (keys)
-  console.log(storefrontApiVersion); // More info -> https://shopify.dev/api/hydrogen/hooks/global/useshop
+  const { languageCode } = useShop(); //useShop hook provides access to values of (hydrogen.config.js) file (keys)
+  console.log(languageCode); // More info -> https://shopify.dev/api/hydrogen/hooks/global/useshop
+  const { data } = useShopQuery({
+    query: QUERY,
+  });
+  console.log(data);
   return (
-    <LocalizationProvider preload="*">
+    <>
       <Header />
       <Suspense fallback={null}>{children}</Suspense>
-    </LocalizationProvider>
+    </>
   );
 };
 
+const QUERY = gql`
+  {
+    shop{
+      name
+    }
+  }
+`;
 export default Layout;
